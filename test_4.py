e85f6d7d23ab53cc0034ee404c9357237091ee43
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-05-13 19:29:51 +0500

    Create test_4.py

---
+++ test_4.py
@@ -0,0 +1,9 @@
+# DESCRIPTION:
+
+# This kata is about multiplying a given number by eight 
+# if it is an even number and by nine otherwise.
+
+# --------------- Answer ---------------
+
+def simple_multiplication(n) :
+    return n * 8 if n % 2 == 0 else n * 9
