25b6bef25e5cdce29059d65f5ccebc489e7a639c
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-05-16 23:29:35 +0500

    Create test_10.py

---
+++ test_10.py
@@ -0,0 +1,11 @@
+# DESCRIPTION:
+# Write a function to split a string and convert it into an array of words.
+
+# Examples (Input ==> Output):
+# "Robin Singh" ==> ["Robin", "Singh"]
+
+# "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+# --------------- Answer ---------------
+
+def string_to_array(s): return s.split(" ")
