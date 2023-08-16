81c330cfba6b4967c171a47249005af8df030079
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-08-16 16:02:20 +0500

    Create test_12.py

---
+++ test_12.py
@@ -0,0 +1,11 @@
+# DESCRIPTION:
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+# Example
+# True  =>  'Yes'
+# False  =>  'No'
+
+# --------------- Answer ---------------
+
+def bool_to_word(bool):
+    return "Yes" if bool else "No"
