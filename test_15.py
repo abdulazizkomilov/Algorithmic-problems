ea939098a3c530c838da4f8b146f31ea2196959c
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-08-29 15:53:07 +0500

    Update test_15.py

--- test_15.py
+++ test_15.py
@@ -2,6 +2,16 @@
 #
 
 # Example
-#   =>
+# -------------------  _     -------- -------
+# [1,2,3,4,5,6,7,8,9], 5  => [1,2,3,4,6,7,8,9]
 
 # --------------- Answer ---------------
+nums = list(range(1, 10))
+print(nums)
+
+
+def pass_num(nums, index):
+    return nums[:index-1] + nums[index:]
+
+
+print(pass_num(nums, 2))
