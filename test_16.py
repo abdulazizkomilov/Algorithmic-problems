ad9562d087c03f4985d13470dd1f66deb4f732fd
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-08-29 15:53:13 +0500

    Create test_16.py

---
+++ test_16.py
@@ -0,0 +1,18 @@
+# DESCRIPTION:
+# input >>> 4
+# output
+# ^^^^^^
+#   16
+#   9
+#   3
+
+# Example
+#
+
+# --------------- Answer ---------------
+
+class Node:
+    def __init__(self, head, left, right):
+        self.head = head
+        self.left = left
+        self.right = right
